57c232391f4f4f4a13c733d0cfa962d9ad7ebb92
Duncan Wallace <duncwa@duncwa.com>
2022-02-19 14:49:47 -0800

    Cleanup add other jenkinsfile and fastfile lane settings

--- fastlane/Jenkinsfile-aos-ins.groovy
+++ fastlane/Jenkinsfile-aos-ins.groovy
@@ -7,7 +7,18 @@
 //  Copyright © 2022. Duncwa LLC.  All rights reserved
 
 pipeline {
-    agent any
+    agent { label "fastlane_ins" }
+
+    options {
+      ansiColor("xterm")
+      timeout(time: 1, unit: "HOURS")
+      buildDiscarder(logRotator(numToKeepStr: "5", artifactNumToKeepStr: "5"))
+    }
+
+    environment {
+      BUILD_NUM = "${env.BUILD_ID}"
+      SLACK_CHANNEL = "${env.SLACK_CHANNEL}"
+    }
 
     stages {
       stage('Setup') {
@@ -17,28 +28,44 @@ pipeline {
               sh 'pwd'
               sh 'echo $PATH'
               sh 'rvm list'
+              sh 'printenv'
           }
       }
-        stage('Build') {
-            steps {
-                echo 'Test PRA'
-                sh 'bundle exec fastlane test_ios_pra'
-            }
+      stage('Inspect for Sensitive Data') {
+          steps {
+            echo 'Run Inspection'
+            sh 'bundle exec fastlane run_aos_ins'
+          }
+          post {
+            always { stash includes: "fastlane/build/**/*", name: "run_ios_ins", allowEmpty: true }
+          }
+      }
+    }
+
+    post {
+      always {
+        script {
+          try { unstash "run_ios_ins" }  catch (e) { echo "Failed to unstash stash: " + e.toString() }
         }
-        stage('Inspect') {
-            steps {
-                echo 'Inspecting..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
+        archiveArtifacts artifacts: "fastlane/build/*dSYM.zip", fingerprint: true
+        archiveArtifacts artifacts: "fastlane/build/*.ipa", fingerprint: true
+      }
+
+      success {
+        sh "echo 'Build Successful' "
+        sh "bundle exec fastlane post_dev_slack_message run_time:${currentBuild.duration / 1000} status:${currentBuild.result}"
+      }
+
+      unstable {
+        sh "echo 'Build Unstable' "
+        sh "bundle exec fastlane post_dev_slack_message run_time:${currentBuild.duration / 1000} status:${currentBuild.result}"
+
+      }
+
+      failure {
+        sh "echo 'Build Failed' "
+        sh "bundle exec fastlane post_dev_slack_message run_time:${currentBuild.duration / 1000} status:${currentBuild.result}"
+      }
+
     }
 }
