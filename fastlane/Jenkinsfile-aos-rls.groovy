6c5fee2d1512ee331fca204795a488f528f20d35
Duncan Wallace <duncwa@duncwa.com>
2022-02-22 19:24:06 -0800

    Prepare for release work and files

--- fastlane/Jenkinsfile-aos-rls.groovy
+++ fastlane/Jenkinsfile-aos-rls.groovy
@@ -3,11 +3,24 @@
 //
 //  Jenkinsfile-aos-rls.groovy
 //  TestingSamples
-//  Created by Duncan Wallace 02/19/2022
+//  Created by Duncan Wallace 02/22/2022
 //  Copyright © 2022. Duncwa LLC.  All rights reserved
 
 pipeline {
-    agent any
+    agent { label "fastlane_pra" }
+
+    options {
+      ansiColor("xterm")
+      timeout(time: 1, unit: "HOURS")
+      buildDiscarder(logRotator(numToKeepStr: "5", artifactNumToKeepStr: "5"))
+    }
+
+    environment {
+      ghprbPullId = "${env.PULL_REQ_NUM}"
+      BUILD_NUM = "${env.BUILD_ID}"
+      SLACK_URL = credentials("s.slackwebhookurl")
+      SLACK_CHANNEL = "${env.SLACK_CHANNEL}"
+    }
 
     stages {
       stage('Setup') {
@@ -19,26 +32,40 @@ pipeline {
               sh 'rvm list'
           }
       }
-        stage('Build') {
-            steps {
-                echo 'Test PRA'
-                sh 'bundle exec fastlane test_ios_pra'
-            }
+      stage('Build and Upload Release APK') {
+          steps {
+              echo 'Generate APK'
+              sh 'bundle exec fastlane generate_rls_apk'
+              sh 'bundle exec fastlane appcenter_upload_rls'
+          }
+          post {
+            always { stash includes: "ui/espresso/BasicSample/app/build/**/*", name: "generate_rls_apk", allowEmpty: true }
+          }
+      }
+    }
+
+    post {
+      always {
+        script {
+          try { unstash "generate_rls_apk" }  catch (e) { echo "Failed to unstash stash: " + e.toString() }
         }
-        stage('Inspect') {
-            steps {
-                echo 'Inspecting..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
+        archiveArtifacts artifacts: "ui/espresso/BasicSample/app/build/**/*", fingerprint: true
+      }
+
+      success {
+        sh "echo 'Build Successful' "
+        sh "bundle exec fastlane post_rls_slack_message run_time:${currentBuild.duration / 1000} status:${currentBuild.result}"
+      }
+
+      unstable {
+        sh "echo 'Build Unstable' "
+        sh "bundle exec fastlane post_rls_slack_message run_time:${currentBuild.duration / 1000} status:${currentBuild.result}"
+
+      }
+
+      failure {
+        sh "echo 'Build Failed' "
+        sh "bundle exec fastlane post_rls_slack_message run_time:${currentBuild.duration / 1000} status:${currentBuild.result}"
+      }
     }
 }
